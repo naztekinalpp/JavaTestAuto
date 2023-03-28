bb0cf8a4d37e1ec460bfa3aaecbc2cd5f7aabaa0
naztekinalp <naz.tekinalp2@hotmail.com>
2023-03-29 01:05:00 +0300

    Implement first part of lecture 4

---
+++ src/ooptask/Cat.java
@@ -0,0 +1,36 @@
+package ooptask;
+
+//fields
+//what can do: method
+//then create constructor
+
+public class Cat {
+    String name;
+    int age;
+
+    Cat(){
+
+    }
+
+    Cat(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+
+    void sleep(){
+
+    }
+
+    void catchMice(){
+
+    }
+
+    void makeSound(){
+
+    }
+
+    void eat(){
+        System.out.println("Cat is eating");
+    }
+}
---
+++ src/ooptask/Dog.java
@@ -0,0 +1,28 @@
+package ooptask;
+
+public class Dog {
+    String name;
+    int age;
+
+    Dog() {
+
+    }
+
+    Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    void sleep() {
+
+    }
+
+    void makeSound() {
+
+    }
+
+    void eat() {
+        System.out.println("Dog is eating");
+    }
+}
